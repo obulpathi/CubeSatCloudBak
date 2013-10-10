3d9c98f99d9e0a9dd5ddf054a4f83135006de7f6
Obulapathi N Challa <obulpathi@gmail.com>
2013-10-10 11:03:39 -0400

    modularizing profiling

--- tests/profiling/profile.py
+++ tests/profiling/profile.py
@@ -5,17 +5,25 @@ import utils
 # profile for read
 def profileRead(chunks):
     readtimes = []
+    readtimes = []
     for chunk in chunks:
         t1 = time.time()
         data = utils.read("chunks/" + chunk)
         t2 = time.time()
         readtimes.append(t2-t1)
+        chunksizes.append(len(data))
     totalReadtime = 0
     for readtime in readtimes:
         totalReadtime = totalReadtime + readtime
     avgReadtime = totalReadtime / len(chunks)
+    totalChunksize = 0
+    for chunksize in chunksizes:
+        totalChunksize = totalChunksize + chunksize
+    avgChunksize = totalChunksize / len(chunksizes)
     print "Average read time:", avgReadtime
     print "Total read time:", totalReadtime
+    print "Average chunk size:", avgChunksize
+    print "Total chunk size:", totalChunksize
     print "Number of chunks read:", len(chunks)
     print "#####################################################"
 
