1a862110ec3ba95d893de92b0c8d07886eadc7f8
Obulapathi N Challa <obulpathi@gmail.com>
2013-10-23 17:05:12 -0400

    modular approach

--- tests/twisted/line/client.py
+++ tests/twisted/line/client.py
@@ -7,19 +7,51 @@ from twisted.protocols.basic import LineReceiver
 
 class EchoClient(LineReceiver):
     def connectionMade(self):
+        self.bits = None
         print("Connection made")
-        self.setRawMode()
-        data = ["a", "b", "c"]
-        self.sendLine(pickle.dumps(data))
-        #filename = "data.jpg"
-        #self.setRawMode()
-        #self.sendLine(open(filename, "r").read())
-        #self.sendLine(data)
+        self.setLineMode()
 
     def connectionLost(self, reason):
         print "connection lost"
         print(reason)
 
+    def lineReceived(self, metadata):
+        print "line received", metadata
+        self.setRawMode()
+        self.receivedMetadata(metadata)
+    
+    def receivedMetadata(self, metadata):
+        self.metadata = metadata
+        self.chunks = []
+        fields = metadata.split(":")
+        self.totalLength = int(fields.pop(0))
+        while fields:
+            self.chunks.append([fields[0], int(fields[1])])
+            fields = fields[2:]
+        
+    def rawDataReceived(self, data):
+        if self.bits:
+            self.bits = self.bits + data
+        else:
+            self.bits = data
+        self.packetReceived()
+        
+    def packetReceived(self):
+        while self.chunks and (len(self.bits) >= self.chunks[0][1]):
+            length = self.chunks[0][1]
+            name = self.chunks[0][0]
+            data = self.bits[:length]
+            self.bits = self.bits[length:]
+            chunk = open("client/" + name, "w")
+            chunk.write(data)
+            chunk.close()
+            self.chunks.pop(0)
+        if not self.chunks:
+            print "YAHOOOOOOOOOOOOOOOOOOOOOOOOOO!"
+        else:
+            print self.chunks
+            
+        
 class EchoFactory(protocol.ClientFactory):
     protocol = EchoClient
 
--- tests/twisted/line/server.py
+++ tests/twisted/line/server.py
@@ -1,14 +1,18 @@
+import os
 import sys
+import math
 import pickle
 
 from twisted.internet import reactor, protocol
 from twisted.protocols.basic import LineReceiver
 
+MAX_PACKET_SIZE = 1000
+
 class Echo(LineReceiver):
     def connectionMade(self):
         print("Connection made")
-        self.setRawMode()
-        #self.setLineMode()
+        self.setLineMode()
+        self.sendData()
 
     def connectionLost(self, reason):
         print("Connection lost")
@@ -16,23 +20,30 @@ class Echo(LineReceiver):
 
     def lineReceived(self, data):
         print "line received"
-        #print(data)
-        """
-        chunk = open("chunk.jpg", "w")
-        chunk.write(data)
-        chunk.close()
-        """
+    
+    def sendMetadata(self):
+        self.sendLine(metadata)
+
+    def sendData(self):
+        metadata = ""
+        data = None
+        chunks = os.listdir("image")
+        for chunk in chunks:
+            fragment = open("image/" + chunk).read()
+            metadata = metadata + ":" + chunk + ":" + str(len(fragment))
+            if data:
+                data = data + fragment
+            else:
+                data = fragment
+        metadata = str(len(data)) + metadata
+        self.sendLine(metadata)
+        self.setRawMode()
+        length = len(data)
+        for i in range(int(math.ceil(float(length)/MAX_PACKET_SIZE))):
+            self.transport.write(data[i*MAX_PACKET_SIZE:(i+1)*MAX_PACKET_SIZE])
+        
     def rawDataReceived(self, data):
         print("raw data received")
-        #print(data)
-        """
-        try:
-            obj = pickle.loads(data)
-            print obj
-        except:
-            print data
-        """
-        #self.transport.write("wa2")
 
 def main():
     """This runs the protocol on port 8000"""
